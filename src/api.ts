229677db9d650e8e9fe23ae7e6a9005e500864a9
NatC02 <NatanCeballos02@gmail.com>
2021-07-01 07:46:52 -0400

    -Decode json web token via frontend app -added validation of current user on the request body -added helper in api.ts to validate user

--- src/api.ts
+++ src/api.ts
@@ -4,6 +4,8 @@ export const app = express();
 import { createStripeCheckoutSession } from './checkout';
 import { createPaymentIntent } from './payments';
 
+import { auth } from './firebase';
+
 app.use(express.json())
 
 import cors from 'cors';
@@ -24,6 +26,42 @@ app.use(
       verify: (req, res, buffer) => (req['rawBody'] = buffer), // works for any endpoint
     })
   );
+
+
+  // Decodes the Firebase JSON Web Token
+app.use(decodeJWT);
+
+/**
+ * Decodes the JSON Web Token sent via the frontend app
+ * Makes the currentUser (firebase) data available on the body.
+ */
+async function decodeJWT(req: Request, res: Response, next: NextFunction) {
+  if (req.headers?.authorization?.startsWith('Bearer ')) {
+    const idToken = req.headers.authorization.split('Bearer ')[1];
+
+    try {
+      const decodedToken = await auth.verifyIdToken(idToken);
+      req['currentUser'] = decodedToken;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  next();
+}
+
+//Helper (the one below is used to validate user or otherwise throw an error)
+
+function validateUser(req: Request) {
+  const user = req['currentUser'];
+  if (!user) {
+    throw new Error(
+      'You must be logged in to make this request. i.e Authroization: Bearer <token>'
+    );
+  }
+
+  return user;
+}
   
 //Webhooks
 
